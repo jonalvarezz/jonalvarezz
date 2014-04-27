3023672db0a5b1d96226da8a9dce90ddfd77ab2f
Jonathan Alvarez González <jonas.al.co@gmail.com>
2014-04-27 05:38:21 -0500

    Adding nib and compressing css output

--- app.js
+++ app.js
@@ -5,10 +5,20 @@ var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var stylus = require('stylus');
+var nib = require('nib');
 
 var routes = require('./routes');
 
 var app = express();
+
+// Setup stylus compile
+function compile(str, path) {
+    return stylus(str)
+        .set('filename', path)
+        .set('compress', true)
+        .use(nib());
+}
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,7 +29,12 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
-app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+
+app.use(stylus.middleware({
+    src: path.join(__dirname, 'public'),
+    compile: compile
+}));
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(app.router);
 
