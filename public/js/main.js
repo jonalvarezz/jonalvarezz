866188c2ad35b5c964c197dd6696f886fa6d6a7a
Jonathan Alvarez <jonas.al.co@gmail.com>
2015-01-03 03:35:17 -0500

    Addding Smooth scrolling

--- public/js/main.js
+++ public/js/main.js
@@ -6,6 +6,33 @@
 
 	'use strict';
 
+	var $window   = $(window),
+	    $document = $(document),
+	    $html     = $(document.documentElement),
+	    $body     = $(document.body),
+	    $surface  = $body,
+	    $content  = $('.content', $surface);
+ 
+	// Smooth scrolling for same page anchors
+	// =====================================================
+    $document.on('click', 'a[href^=#]:not([href=#])', function(e) {
+      e.preventDefault();
+ 
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $surface.animate({
+          scrollTop: target.offset().top
+        }, 500);
+        location.hash = this.hash;
+        return false;
+      }
+    });
+ 
+    // Fix oveflow-scrolling on iOS7
+    $surface.on('touchstart', function(e) {});
+
+
 	// Make the hero height same as the viewport height
 	// =====================================================
 	var parent = $('#hi');
